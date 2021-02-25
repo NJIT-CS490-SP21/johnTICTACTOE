f79c73cd0cfda8de193bc78d497e6ceafdd5e650
John-Salerno <jjs77@njit.edu>
2021-02-25 20:22:40 +0000

    added logic to board.js so that only player x and player o can play alternating between the two correctly, anyone who joins after cant. also added list of users at bottom, this will be updated in the future to be nicer.

--- src/Board.js
+++ src/Board.js
@@ -12,15 +12,33 @@ export function Board(){
     const [userList, setUserList] = useState([]);
     const [username, setUsername] = useState(null);
     const [isShown, setShown] = useState(false);
+    const [moveCount, setCount] = useState(1); //odd = x, even = o
 
     function onClickSquare(index) {
         if (board[index] != null) {
             return;
         }
+        
+        let move = "ERROR";
+        if (username === userList[0] && moveCount%2===1) {
+            move = "X";
+            setCount((prevCount) => {
+                return prevCount+1;
+            });
+        } else if (username === userList[1] && moveCount%2===0) {
+            move = "O";
+            setCount((prevCount) => {
+                return prevCount+1;
+            });
+        } else {
+            console.log(moveCount);
+            return;
+        }
+        
         setBoard(prevBoard => {
             const boardCopy = [...prevBoard];
-            boardCopy[index] = "X";
-            socket.emit('boardMove', { message: index });
+            boardCopy[index] = move;
+            socket.emit('boardMove', { index: index, move: move, count: moveCount+1});
             return boardCopy;
         });
     }
@@ -35,9 +53,7 @@ export function Board(){
                 return !prevShown;
             });
             
-            socket.emit('login', { message: newUsername});
-            
-            
+            socket.emit('login', { uName: newUsername});
         }
     }
     
@@ -45,17 +61,20 @@ export function Board(){
         socket.on('boardMove', (data) => {
             console.log('move has been received');
             console.log(data);
+            
+            setCount(data.count);
+            
             setBoard(prevBoard => {
-            const boardCopy = [...prevBoard];
-            boardCopy[data.message] = "O";
-            return boardCopy;
+                const boardCopy = [...prevBoard];
+                boardCopy[data.index] = data.move;
+                return boardCopy;
             });
         });
         
         socket.on('login', (data) => {
             console.log('user has logged in');
             console.log(data);
-            setUserList(prevList => [...prevList, data.message]);
+            setUserList(prevList => [...prevList, data.uName]);
         });
     }, []);
     
@@ -63,14 +82,26 @@ export function Board(){
     if (isShown) {
         return (
             <div>
+                <h1> Welcome to Tic Tac Toe! </h1>
                 <div class="mainBoard">
                     {board.map((box, index) => (<Box onClick={() => onClickSquare(index)} letter={box} />))}
+                </div>
+                <div>
+                    <h3> List of current players: </h3>
+                    {userList.map((uName) => {
+                        if (uName === username) {
+                            return <div>{uName} (You)</div>; 
+                        } else {
+                            return <div>{uName}</div>;
+                        }
+                    })}
                 </div>
             </div>
         );
     } else {
        return (
             <div>
+                <h1> Enter a username before playing </h1>
                 <input ref={inputRef} type="text" />
                 <button onClick={onClickButton}>Enter Username</button>
             </div>
